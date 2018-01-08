3a6a61980229ad4c3baadf3730669dea20f9ceaa
opus1269 <igreilly1269@gmail.com>
2018-01-08 16:18:13 -0600

    add optional parameters to removeCachedToken

--- scripts/auth.js
+++ scripts/auth.js
@@ -20,31 +20,44 @@ Chrome.Auth = (function() {
 
   return {
     /**
-     * Get our OAuth2.0 token
+     * Get an OAuth2.0 token<br />
+     * Note: Every time you use a different scopes array, you will get a new
+     * token the first time, so you need to always get it with those scopes
+     * and remove the cached one with the scopes.
      * @param {boolean} interactive - if true may block
+     * @param {string[]|null} [scopes=null] - optional scopes to use, overrides
+     * those in the manifest
      * @returns {Promise<string>} An access token
      * @memberOf Chrome.Auth
      */
-    getToken: function(interactive = false) {
-      return chromep.identity.getAuthToken({
-        'interactive': interactive,
-      }).then((token) => {
+    getToken: function(interactive = false, scopes = null) {
+      const request = {
+        interactive: interactive,
+      };
+      if (scopes && scopes.length) {
+        request.scopes = scopes;
+      }
+      return chromep.identity.getAuthToken(request).then((token) => {
         return Promise.resolve(token);
       });
     },
 
     /**
-     * Remove our cached auth token
+     * Remove a cached OAuth2.0 token
+     * @param {boolean} interactive - if true may block
      * @param {string|null} [curToken=null] token to remove
+     * @param {string[]|null} [scopes=null] - optional scopes to use, overrides
+     * those in the manifest
      * @returns {Promise.<string>} the old token
      * @memberOf Chrome.Auth
      */
-    removeCachedToken: function(curToken = null) {
+    removeCachedToken: function(interactive = false, curToken = null,
+                                scopes = null) {
       let oldToken = null;
       if (curToken === null) {
-        return this.getToken(false).then((token) => {
+        return this.getToken(interactive, scopes).then((token) => {
           oldToken = token;
-          return chromep.identity.removeCachedAuthToken({'token': token});
+          return chromep.identity.removeCachedAuthToken({token: token});
         }).then(() => {
           return Promise.resolve(oldToken);
         });
